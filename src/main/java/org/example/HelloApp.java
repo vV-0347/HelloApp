1cc211dbee9d9ba55c0c45ffe4cb1e2dae631296
vV-0347 <vidyuthsumanth1@gmail.com>
2026-03-26 08:43:37 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,9 +1,10 @@
 package org.example;
 
 public class HelloApp {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+            String name = args[0];
+
+            System.out.println("Hello, " + name + "!");
+    }
 }
